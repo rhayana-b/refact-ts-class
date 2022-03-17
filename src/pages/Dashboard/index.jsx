81f1d82b507a0ef31a07007ece2cfc01055cba83
Rhayana Barbosa <rhayanabarbosa@gmail.com>
2022-03-16 22:11:46 -0300

    fix imports

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,11 +1,11 @@
 import { Component } from 'react';
 
-import Header from '../../components/Header';
-import api from '../../services/api';
-import Food from '../../components/Food';
+import { Header } from '../../components/Header';
+import { Food } from '../../components/Food';
+import { FoodsContainer } from './styles';
 import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
-import { FoodsContainer } from './styles';
+import api from '../../services/api';
 
 class Dashboard extends Component {
   constructor(props) {
